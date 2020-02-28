ee7ac160d4257c35a358573ecb31dbe8a6aa1adf
Jeremy <jgertz26@gmail.com>
2020-02-28 16:15:13 -0500

    Add input boxes that don't do anything particularly exciting

--- src/App.js
+++ src/App.js
@@ -12,13 +12,23 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('Was clicked')
     this.setState({ persons:
       [
         { name: 'Max', age: 28 },
         { name: 'Manu', age: 29 },
-        { name: 'Jeremy', age: 29 }
+        { name: newName, age: 29 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({ persons:
+      [
+        { name: 'Max', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: event.target.value, age: 29 }
       ]
     })
   }
@@ -28,10 +38,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is working wowowow</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>BLAHHHH!</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Jeremy')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}>BLAHHHH!</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, "JEREREMY")}
+          changed={this.nameChangedHandler}/>
       </div>
     );
   }
