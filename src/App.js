cb79872c7677223000e188da86b47aaf5ec1db8a
Jeremy <jgertz26@gmail.com>
2020-02-28 15:36:07 -0500

    Add switch name button, give it an event handler that alters the state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import Person from './Person/Person'
 
 class App extends Component {
 
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Diddly', age: 14 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked')
+    this.setState({ persons:
+      [
+        { name: 'Max', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Jeremy', age: 29 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is working wowowow</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">I LIKE THINGS!</Person>
-        <Person name="Diddly" age="14"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>BLAHHHH!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
